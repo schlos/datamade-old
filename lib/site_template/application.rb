0036a5bd19696f6b9ca19e0a9cc8b265d15b6b3e
Derek Eder <derek.eder@gmail.com>
2013-02-08 01:17:32 -0600

    removed derekeder stuff, started formatting datamade stuff

--- lib/site_template/application.rb
+++ lib/site_template/application.rb
@@ -27,67 +27,8 @@ module SiteTemplate
     get "/" do
       cache_control :public, max_age: 3600  # 1 hour
       @current_menu = "home"
-      @title = 'Derek Eder - Open Data Web Developer'
+      @title = 'DataMade - Tell a story with your data'
       haml :index
-    end
-
-    # redirects
-    get "/consulting/?" do
-      redirect "/datamade"
-    end
-
-    get "/budget*" do
-      redirect "http://lookatcook.com"
-    end
-
-    get "/clearstreets*" do
-      redirect "http://clearstreets.org"
-    end
-
-    get "/cps-tiers*" do
-      redirect "http://cpstiers.opencityapps.org"
-    end
-
-    get "/maps/chicago-abandoned-buildings/*" do
-      redirect "http://chicagobuildings.org"
-    end
-
-    get "/tif-map*" do
-      redirect "/maps/chicago-tif/"
-    end
-
-    get "/maps/chicago-clinics/table/" do
-      redirect "http://old.derekeder.com/maps/chicago-clinics/table/"
-    end
-
-    get "/maps/:map/" do
-      redirect "/maps/#{params[:map]}/index.html"
-    end
-
-    #blog
-    get "/blog/?*" do
-      jekyll_blog(request.path) {404}
-    end
-
-    def jekyll_blog(path, &missing_file_block)
-      @current_menu = "blog"
-      @title = "Blog - Derek Eder"
-
-      file_path = File.join(File.dirname(__FILE__), 'jekyll_blog/_site',  path.gsub('/blog',''))
-      file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i  
-      if File.exist?(file_path)
-        file = File.open(file_path, "rb")
-        contents = file.read
-        file.close
-
-        if (file_path.include?('.xml') || file_path.include?('.css'))
-          erb contents, :content_type => 'text/xml'
-        else
-          erb contents, :layout_engine => :haml
-        end
-      else
-        haml :not_found
-      end
     end
     
     # catchall for static pages
@@ -95,7 +36,7 @@ module SiteTemplate
       cache_control :public, max_age: 3600  # 1 hour
       begin 
         @current_menu = params[:page]
-        @title = params[:page].capitalize.gsub(/[_-]/, " ") + " - Derek Eder"
+        @title = params[:page].capitalize.gsub(/[_-]/, " ") + " - DataMade"
         @page_path = params[:page]
         haml params[:page].to_sym
       rescue Errno::ENOENT
